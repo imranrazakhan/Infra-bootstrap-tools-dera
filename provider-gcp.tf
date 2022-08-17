a9a5ee69195c98bf27c96cd052245da11264df4e
imranrazakhan <imranrazakhan@users.noreply.github.com>
2022-08-17 18:53:06 +0200

    initial commit

--- provider-gcp.tf
+++ provider-gcp.tf
@@ -2,3 +2,22 @@ provider "google" {
   project     = var.project_id
   region      = var.region
 }
+
+# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/integer
+resource "random_integer" "int" {
+  min = 100
+  max = 1000000
+}
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 3.66"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1"
+    }
+  }
+}
